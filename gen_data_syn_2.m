d0e1b43a37534d98b7bbf884d9e935adf9c1b5d0
Yi Li <liyi.1989@gmail.com>
2014-10-16 15:08:43 -0400

    hello

--- gen_data_syn_2.m
+++ gen_data_syn_2.m
@@ -29,12 +29,11 @@ for idx_run = 1:n_runs
     % Y is the M function of X1
     %Y = M_function(X1);
     Y = X1;
+    %Y=4.*(X1-0.5).^2;
     
     % X2 = X1 with p=0.8 and X4 = uniform noise otherwise
     X2 = zeros(n_samples,1);
-    idx_sel = false(n_samples,1);
-    tmp = randsample(n_samples,floor(0.7*n_samples));
-    idx_sel(tmp) = true;
+    idx_sel=datasample([false true],n_samples,'Weight',[0.3 0.7]);
     X2(idx_sel) = X1(idx_sel);
     X2(~idx_sel) = rand(sum(~idx_sel),1);
     
@@ -44,20 +43,19 @@ for idx_run = 1:n_runs
     
     % X4 = X1 with p=0.2 and X4 = uniform noise otherwise
     X4 = zeros(n_samples,1);
-    idx_sel = false(n_samples,1);
-    tmp = randsample(n_samples,floor(0.3*n_samples));
-    idx_sel(tmp) = true;
+    idx_sel=datasample([false true],n_samples,'Weight',[0.7 0.3]);
     X4(idx_sel) = X1(idx_sel);
     X4(~idx_sel) = rand(sum(~idx_sel),1);
     
     % Compute MI and Ccor
     X = [X1,X2,X3,X4];
+    %X=tiedrank(X);
     % compute kernel matrix for each feature
     options_Y.KernelType = 'Gaussian';
     options_Y.t = optSigma(Y);
     mtr_kernel_Y = constructKernel(Y,Y,options_Y);
     for j = 1:4
-        disp(j);
+        %disp(j);
         %val_mi(idx_run,j) = statistic_mi_kraskov(X(:,j),Y,5);
         %val_ccor(idx_run,j) = ccore_tie(X(:,j),Y);
         options_X.KernelType = 'Gaussian';
